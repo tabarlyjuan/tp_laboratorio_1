33ab63d4baed8b075ed71ae57849a5e3a3fd4083
tabarlyjuan <juan.tabarly@hotmail.com>
2017-04-05 14:57:38 -0300

    calculadora
    calculadora

--- funciones.h
+++ funciones.h
@@ -1,14 +1,18 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
-float IngresarNumero ();
-float sumar (float x, float y);
-float restar (float x, float y);
-float dividir (float x, float y);
-float multiplicar (float x,float y);
-float factorizar (float x);
+
+float sumar (float, float);
 
 
+float restar (float, float);
 
 
+float dividir(float, float);
+
+
+float multiplicar (float, float);
+
+
+float factorial(float);
 
 #endif // FUNCIONES_H_INCLUDED
--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funciones.h"
 #include <ctype.h>
+#include "funciones.h"
 
 int main()
 {
-    float x = 0, y = 0, suma, resta, producto, cociente;
     char seguir='s';
-    int opcion=0, factorial;
+    int opcion=0;
+    float resultado,num1, num2;
+    char x = 'x';
+    char y = 'y';
 
     while(seguir=='s')
     {
-        printf("1- Ingresar 1er operando (A=%f)\n", x);
-        printf("2- Ingresar 2do operando (B=%f)\n", y);
+        printf("1- Ingresar 1er operando (A = %c)\n",x);
+        printf("2- Ingresar 2do operando (B = %c)\n",y);
         printf("3- Calcular la suma (A+B)\n");
         printf("4- Calcular la resta (A-B)\n");
         printf("5- Calcular la division (A/B)\n");
@@ -22,108 +24,121 @@ int main()
         printf("9- Salir\n");
 
         scanf("%d",&opcion);
-
-        system("cls");
+        fflush(stdin);
 
         switch(opcion)
         {
             case 1:
-                x = IngresarNumero(x);
+                printf("ingrese operando: ");
+                scanf("%c", &x);
+                fflush(stdin);
+                system("cls");
 
+                while(isdigit(x) == 0)
+                {
+                    printf("REingrese operando: ");
+                    scanf("%c", &x);
+                    fflush(stdin);
+                    system("cls");
+                }
+            num1 = (float)(x-48);
                 break;
-
             case 2:
-               y = IngresarNumero(x);
+               printf("ingrese operando: ");
+                scanf("%c", &y);
+                fflush(stdin);
+                system("cls");
+
+                while(isdigit(y) == 0)
+                {
+                    printf("REingrese operando: ");
+                    scanf("%c", &y);
+                    fflush(stdin);
+                    system("cls");
+                }
+            num2 = (float) (y-48);
                 break;
-
-
             case 3:
-                suma = sumar (x, y);
-                printf("\n la suma da; %f\n", suma);
+                resultado = sumar(num1, num2);
+                printf("La suma nos da: %f\n", resultado);
                 break;
-
-
             case 4:
-
-                resta = restar (x, y);
-                printf("\n La diferencia de los operandos es : %f \n", resta);
+                resultado = restar(num1, num2);
+                printf("La diferencia nos da: %f\n", resultado);
                 break;
-
-
             case 5:
-
-                   cociente = dividir ( x, y);
-                printf("\n  El cociente de la division es : %f \n", cociente);
-
-
+                if (num2 == 0)
+                {
+                    printf("No se puede dividir por cero\n");
+                }
+                else
+                {
+                    resultado = dividir(num1, num2);
+                    printf("El cociente nos da: %f\n", resultado);
+                }
                 break;
-
-
             case 6:
-
-
-               producto = multiplicar (x,y);
-                printf(" \n  El producto de los operadores es : %f \n", producto);
-
+                 resultado = multiplicar (num1, num2);
+                 printf("El producto nos da: %f\n", resultado);
 
                 break;
             case 7:
-
-            factorial = factorizar (x);
-
-            if (factorial >= 1)
-            {
-               printf("\n el factorial es %i \n", factorial);
-            }
-            else
-            {
-                printf("\n MathError! \n");
-            }
-
-
-
+                if(num1 < 0)
+                {
+                    printf("No se puede calcular el factorial de un numero negativo\n");
+                }
+                else if(num1 == 0)
+                {
+                    printf("Por definicion el factorial de cero es uno\n");
+                }
+                else
+                {
+                    resultado = factorial(num1);
+                    printf("El factorial nos da: %f\n", resultado);
+                }
                 break;
-
             case 8:
+                //suma
+                 resultado = sumar(num1, num2);
+                printf("La suma nos da: %f\n", resultado);
+                //resta
+                 resultado = restar(num1, num2);
+                printf("La diferencia nos da: %f\n", resultado);
+                //division
+                if (num2 == 0)
+                {
+                    printf("No se puede dividir por cero\n");
+                }
+                else
+                {
+                    resultado = dividir(num1, num2);
+                    printf("El cociente nos da: %f\n", resultado);
+                }
+                //multiplicacion
+                 resultado = multiplicar (num1, num2);
+                 printf("El producto nos da: %f\n", resultado);
 
-               suma = sumar (x, y);
-                printf("\n La suma da; %f\n", suma);
-
-                  resta = restar (x, y);
-                printf("\n La diferencia de los operandos es : %f \n", resta);
-
-                 cociente = dividir ( x, y);
-                printf("\n El cociente de la division es : %f \n", cociente);
-
-                 producto = multiplicar (x,y);
-                printf(" \n El producto de los operadores es : %f \n", producto);
-
-                  factorial = factorizar (x);
-
-            if (factorial >= 1)
-            {
-               printf("\n El factorial es %i \n", factorial);
-            }
-            else
-            {
-                printf("\n MathError! \n");
-            }
-
-
-
-
-
-
+                //factorial
+                if(num1 < 0)
+                {
+                    printf("No se puede calcular el factorial de un numero negativo\n");
+                }
+                else if(num1 == 0)
+                {
+                    printf("Por definicion el factorial de cero es uno\n");
+                }
+                else
+                {
+                    resultado = factorial(num1);
+                    printf("El factorial nos da: %f\n", resultado);
+                }
 
                 break;
             case 9:
                 seguir = 'n';
                 break;
         }
-
     }
-
-
 
     return 0;
 }
