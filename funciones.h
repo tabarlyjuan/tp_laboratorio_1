75dddd47a4236b321656c6c0dc67f912e20ff3a0
tabarlyjuan <juan.tabarly@hotmail.com>
2017-04-07 11:23:50 -0300

    comentar
    documentado.

--- funciones.h
+++ funciones.h
@@ -1,17 +1,53 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
+/** \brief se encarga de realizar la suma de dos numeros.
+ *
+ * \param numero de tipo float.
+ * \param numero de tipo float.
+ * \return el resultado de la suma.
+ *
+ */
+
 float sumar (float, float);
 
-
+/** \brief se encarga de realizar la resta de dos numeros.
+ *
+ * \param numero de tipo float.
+ * \param numero de tipo float.
+ * \return el resultado de la diferencia.
+ *
+ */
 float restar (float, float);
 
+/** \brief realiza el cociente entre dos numeros.
+ *
+ * \param numero de tipo float.
+ * \param numero de tipo float.
+ * \return el cociente obtenido.
+ *
+ */
 
 float dividir(float, float);
+
+/** \brief realiza el producto entre dos numeros.
+ *
+ * \param numero de tipo float.
+ * \param numero de tipo float.
+ * \return el producto obtenido.
+ *
+ */
 
 
 float multiplicar (float, float);
 
+/** \brief realiza el factorial del numero ingresado.
+ *
+ * \param toma un numero de tipo entero.
+ *
+ * \return el numero obtenido de realizar dicha operacion.
+ *
+ */
 
 float factorial(float);
 
